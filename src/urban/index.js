a8cde8d4d2869edc7f484625c075f9508578a911
CyberArtsBR <meltbh@Hotmail.com>
2026-09-25 03:07:46 -0300

    Export urban environment component API

---
+++ src/urban/index.js
@@ -0,0 +1,13 @@
+export {createUrbanMaterials} from './urbanMaterials.js';
+export {
+  URBAN_ENVIRONMENT_DEFAULTS,
+  createAsphaltRoadSurface,
+  createStreetMarkings,
+  createStreetlights,
+  createBuildingSkyline,
+  createTrafficCones,
+  createBarriers,
+  createRoadSigns,
+  createUrbanRoadsideScenery,
+  createUrbanEnvironment
+} from './urbanEnvironment.js';

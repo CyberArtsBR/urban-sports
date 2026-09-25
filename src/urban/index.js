65cc3b72d81f981d49aadc1df7772949bffe3755
CyberArtsBR <meltbh@Hotmail.com>
2026-09-25 03:44:19 -0300

    feat(urban): export gameplay obstacle factories

--- src/urban/index.js
+++ src/urban/index.js
@@ -11,3 +11,17 @@ export {
   createUrbanRoadsideScenery,
   createUrbanEnvironment
 } from './urbanEnvironment.js';
+
+export {
+  URBAN_OBSTACLE_DIMENSIONS,
+  URBAN_OBSTACLE_MAPPING,
+  URBAN_OBSTACLE_FACTORIES,
+  createUrbanObstacle,
+  createUrbanObstaclePrototypes,
+  createTrafficConeObstacle,
+  createConcreteBarrierObstacle,
+  createConstructionBarricadeObstacle,
+  createWideConstructionBarricadeObstacle,
+  createUrbanRampObstacle,
+  createUrbanOilHazard
+} from './urbanObstacles.js';
